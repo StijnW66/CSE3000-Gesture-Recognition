129b43b51385f07b19aed9d5a04a9917eee774b0
mitko <mbarantiev@gmail.com>
2022-05-09 19:01:12 +0200

    FixedLength: Simple linear interpolation.

---
+++ src/DiodeReader/SimpleSignalStretcher.h
@@ -0,0 +1,26 @@
+#include<inttypes.h>
+#include<Arduino.h>
+
+class SimpleSignalStretcher {
+
+    public:
+        SimpleSignalStretcher() {}
+    
+        void StretchSignal(float * signal, int sLength, float * dest, int dLength) {
+
+            int i = 0;
+            float coeff = (float)(sLength-1) / (dLength-1);
+
+            while(i < dLength) {
+
+                float index = i == 0 ? i : i == dLength - 1 ? sLength - 1 : i * coeff;
+                int low = (int)index;
+                int high = ceil(index);
+
+                dest[i] = (index - low) * signal[high] + (low + 1 - index) * signal[low];
+
+                i++;
+            }
+
+        }
+};
