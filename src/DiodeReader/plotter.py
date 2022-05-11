4cd33bddadb589d614951f9bb1130bd206e20e22
mitko <mbarantiev@gmail.com>
2022-05-11 14:24:18 +0200

    Plotting: Make a 4-ax figure.

--- src/DiodeReader/plotter.py
+++ src/DiodeReader/plotter.py
@@ -6,11 +6,9 @@ ser = serial.Serial('/dev/ttyACM0')
 
 print(ser.name)
 
-while True:
-
+def readData():
     data1 = []
     data2 = []
-
     while ser.readline() != b"Start\r\n":
         pass
 
@@ -24,12 +22,31 @@ while True:
             data1.append(d[0])
             data2.append(d[1])
     
-    # plot
+    return (data1, data2)
+
+def plotData(ax, data1, data2, title):
     l = len(data1)
     xs = list(np.arange(0, l))
-    ys = data1
 
-    plt.plot(xs, data1, data2)
+    ax.plot(xs, data1, data2)
+    ax.set_title(title)
+
+while True:
+    
+    initData1, initData2 = readData()
+    smothData1, smothData2 = readData()
+    normData1, normData2 = readData()
+    streData1, streData2 = readData()
+    
+    # plot
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2,2)
+    fig.suptitle("PhotoDiode Data")
+
+    plotData(ax1, initData1, initData2, "Raw Data")
+    plotData(ax2, smothData1, smothData2, "Filtered Data")
+    plotData(ax3, normData1, normData2, "Normalised Data")
+    plotData(ax4, streData1, streData2, "Time-Stretched Data")
+
     plt.show()
 
 ser.close()
