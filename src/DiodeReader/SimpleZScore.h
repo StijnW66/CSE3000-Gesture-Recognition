7e0b55b0f627e32b382e361c44fba2bc0d9432e4
mitko <mbarantiev@gmail.com>
2022-05-12 19:50:04 +0200

    Normalisation: Ignore 0s.

--- src/DiodeReader/SimpleZScore.h
+++ src/DiodeReader/SimpleZScore.h
@@ -9,14 +9,22 @@ class SimpleZScore {
         void ComputeZScore(float * signal, int length, bool to1) {
             float mean = 0, stdev = 0;
             int index = 0;
+            int non0Elements = 0;
 
             // Mean
-            while(index++ < length) mean += signal[index];
-            mean /= length;
+            while(index++ < length) {
+                if (signal[index] != 0) {
+                    mean += signal[index];
+                    non0Elements++;
+                }
+            }
+            mean /= non0Elements;
 
             // Compute stddev
             index = 0;
-            while(index++ < length) stdev += pow(signal[index] - mean, 2);
+            while(index++ < length) 
+                if (signal[index] != 0)
+                    stdev += pow(signal[index] - mean, 2);
             stdev = pow(stdev, 0.5);
 
             // Compute the Z-score and put it in the destination
@@ -24,41 +32,56 @@ class SimpleZScore {
             if (to1) {
                 float max = 0;
                 while(index < length) {
-                    signal[index] = ((signal[index] - mean) / stdev + 1) / 2.0f;
+                    if (signal[index] != 0)
+                        signal[index] = ((signal[index] - mean) / stdev + 1) / 2.0f;
+    
                     if (signal[index] > max) max = signal[index]; 
+    
                     index++;
                 }
                 index = 0;
-                while(index < length) {
-                    signal[index] /= max;
-                    index++;
-                }
+                // while(index < length) {
+                //     signal[index] /= max;
+                //     index++;
+                // }
             }
             else 
                 while(index++ < length) {
-                    signal[index] = (signal[index] - mean) / stdev;
+                    if (signal[index] != 0)
+                        signal[index] = (signal[index] - mean) / stdev;
                     index++;
                 }
-            
-
         }
 
         void ComputeZScore(float * signal1, float * signal2, int length, bool to1) {
             float mean = 0, stdev = 0;
             int index = 0;
+            int non0Elements = 0;
 
             // Mean
             while(index < length) {
-                mean += (signal1[index] + signal2[index]);
+                if(signal1[index] != 0) {
+                    mean += signal1[index];
+                    non0Elements++;
+                }
+                if(signal2[index] != 0) {
+                    mean += signal2[index];
+                    non0Elements++;
+                }
                 index++;
             }
 
-            mean /= (2*length);
+            mean /= non0Elements;
 
             // Compute stddev
             index = 0;
             while(index < length) {
-                stdev += (pow(signal1[index] - mean, 2) + pow(signal2[index] - mean, 2));
+                if(signal1[index] != 0) {
+                    stdev += pow(signal1[index] - mean, 2);
+                }
+                if(signal2[index] != 0) {
+                    stdev += pow(signal2[index] - mean, 2);
+                }
                 index++;
             }
             stdev = pow(stdev, 0.5);
@@ -67,14 +90,18 @@ class SimpleZScore {
             index = 0;
             if (to1)
                 while(index < length) {
-                    signal1[index] = ((signal1[index] - mean) / stdev + 1) / 2.0f;
-                    signal2[index] = ((signal2[index] - mean) / stdev + 1) / 2.0f;                    
+                    if(signal1[index] != 0)
+                        signal1[index] = ((signal1[index] - mean) / stdev + 1) / 2.0f;
+                    if(signal2[index] != 0) 
+                        signal2[index] = ((signal2[index] - mean) / stdev + 1) / 2.0f;                    
                     index++;
                 }
             else 
                 while(index < length) {
-                    signal1[index] = (signal1[index] - mean) / stdev;
-                    signal2[index] = (signal2[index] - mean) / stdev;
+                    if(signal1[index] != 0)
+                        signal1[index] = (signal1[index] - mean) / stdev;
+                    if(signal2[index] != 0) 
+                        signal2[index] = (signal2[index] - mean) / stdev;                
                     index++;
                 }
         }
