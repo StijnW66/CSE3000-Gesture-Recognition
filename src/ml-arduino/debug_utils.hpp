1ea4239b72a1e5f526a083106a8fb29795ceb6f0
William Narchi <willy.narchi@gmail.com>
2022-05-24 22:36:04 +0200

    Place debugging methods in separate file

---
+++ src/ml-arduino/debug_utils.hpp
@@ -0,0 +1,43 @@
+#ifndef ML_ARDUINO_DEBUG_UTILS_H_
+#define ML_ARDUINO_DEBUG_UTILS_H_
+
+#include "dummy_data.hpp"
+#include "main_arduino.hpp"
+
+/**
+ * Print the dimensions of the input and output tensors to serial.
+ */
+void printTensorDimensionsToSerial() {
+  char input_prop_str[96];
+  sprintf(input_prop_str, "Input dimensions: %d - (%d, %d, %d, %d)",
+    input->dims->size,
+    input->dims->data[0],
+    input->dims->data[1],
+    input->dims->data[2],
+    input->dims->data[3]);
+  Serial.println(input_prop_str);
+  char output_prop_str[96];
+  sprintf(output_prop_str, "Output dimensions: %d - (%d, %d)",
+    output->dims->size,
+    output->dims->data[0],
+    output->dims->data[1]);
+  Serial.println(output_prop_str);
+}
+
+/**
+ * Print the results of running the CNN on dummy data (see dummy_data.hpp)
+ */
+void printDummyDataInferenceResults() {
+  Gesture rot_clockwise_inference_0 = inferGesture2d(ROT_CLOCKWISE_DUMMY_0);
+  Gesture rot_clockwise_inference_1 = inferGesture2d(ROT_CLOCKWISE_DUMMY_1);
+  Gesture swipe_up_inference_0 = inferGesture2d(SWIPE_UP_DUMMY_0);
+
+  char inference_results_str[32];
+  sprintf(inference_results_str, "(%d, %d, %d)",
+    rot_clockwise_inference_0,
+    rot_clockwise_inference_1,
+    swipe_up_inference_0);
+  Serial.println(inference_results_str);
+}
+
+#endif
--- src/ml-arduino/main_arduino.hpp
+++ src/ml-arduino/main_arduino.hpp
@@ -4,7 +4,6 @@
 #include <TensorFlowLite.h>
 
 #include "constants.hpp"
-#include "dummy_data.hpp"
 #include "model_quantization.h"
 #include "prediction_enums.hpp"
 #include "../DiodeReader/parameters.h"
@@ -25,7 +24,7 @@ TfLiteTensor* output = nullptr;
 
 constexpr size_t kTensorArenaSize = 15U * 1024U;
 uint8_t tensor_arena[kTensorArenaSize];
-}  // namespace
+}
 
 void tensorflowSetup() {
   // Set up logging. Google style is to avoid globals or statics because of
@@ -94,6 +93,7 @@ static Gesture gestureHighestProbability() {
  * @param signal A 2D array where each row represents the readings of a single photodiode
  * over time. Expected dimensions are (NUM_PDs x ML_DATA_LENGTH), see DiodeReader/parameters.h
  * 
+ * @return An enum indicating which gesture was recognised (see prediction_enums.hpp)
  */
 Gesture inferGesture2d(float signal[NUM_PDs][ML_DATA_LENGTH]) {
   // Re-order data to fit expected input shape (in effect, it's transposed)
@@ -113,42 +113,6 @@ Gesture inferGesture2d(float signal[NUM_PDs][ML_DATA_LENGTH]) {
   }
   
   return gestureHighestProbability();
-}
-
-/**
- * Print the dimensions of the input and output tensors to serial.
- */
-void printTensorDimensionsToSerial() {
-  char input_prop_str[96];
-  sprintf(input_prop_str, "Input dimensions: %d - (%d, %d, %d, %d)",
-    input->dims->size,
-    input->dims->data[0],
-    input->dims->data[1],
-    input->dims->data[2],
-    input->dims->data[3]);
-  Serial.println(input_prop_str);
-  char output_prop_str[96];
-  sprintf(output_prop_str, "Output dimensions: %d - (%d, %d)",
-    output->dims->size,
-    output->dims->data[0],
-    output->dims->data[1]);
-  Serial.println(output_prop_str);
-}
-
-/**
- * Print the results of running the CNN on dummy data (see dummy_data.hpp)
- */
-void printDummyDataInferenceResults() {
-  Gesture rot_clockwise_inference_0 = inferGesture2d(ROT_CLOCKWISE_DUMMY_0);
-  Gesture rot_clockwise_inference_1 = inferGesture2d(ROT_CLOCKWISE_DUMMY_1);
-  Gesture swipe_up_inference_0 = inferGesture2d(SWIPE_UP_DUMMY_0);
-
-  char inference_results_str[32];
-  sprintf(inference_results_str, "(%d, %d, %d)",
-    rot_clockwise_inference_0,
-    rot_clockwise_inference_1,
-    swipe_up_inference_0);
-  Serial.println(inference_results_str);
 }
 
 #endif
