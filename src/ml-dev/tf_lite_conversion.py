489a0bc0a082c556a6bec1243d1260196f94aaff
William Narchi <willy.narchi@gmail.com>
2022-06-08 16:37:09 +0200

    Switch to full quantization (I/O incl.) without FLOAT32 fallback

--- src/ml-dev/tf_lite_conversion.py
+++ src/ml-dev/tf_lite_conversion.py
@@ -12,10 +12,14 @@ representative_dataset = None # TODO: Devise less hacky solution
 def _convert_no_optimisations(model: tf.keras.Model, write_to_file: bool = True):
     """
     Convert a Keras model to a TensorflowLite model without applying any additional
-    optimisations and save it to a file.
+    optimisations and (optionally) save it to a file.
 
     Args:
         model: The (trained) model to be converted
+        write_to_file: Whether to save the converted model to a file
+
+    Returns:
+        The converted TFLite model in serialised format
     """
     converter = tf.lite.TFLiteConverter.from_keras_model(model)
     tflite_model = converter.convert()
@@ -28,15 +32,22 @@ def _convert_with_quantization(model: tf.keras.Model, dataset: np.ndarray,
                                write_to_file: bool = True):
     """
     Convert a Keras model to a TensorflowLite model, applying default optimisations
-    and quantization, and save it to a file.
+    and quantization (incl. I/O quantization), and (optionally) save it to a file.
 
     Args:
         model: The (trained) model to be converted
         dataset: The dataset used to test and/or train the model. Used to infer
         representative input values for quantization
+        write_to_file: Whether to save the converted model to a file
+
+    Returns:
+        The converted TFLite model in serialised format
     """
     converter = tf.lite.TFLiteConverter.from_keras_model(model)
     converter.optimizations = [tf.lite.Optimize.DEFAULT]
+    converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+    converter.inference_input_type = tf.uint8
+    converter.inference_output_type = tf.uint8
 
     # Create representative values for quantization
     representative_dataset = dataset
@@ -57,12 +68,16 @@ def _convert_with_quantization_aware_training(model: tf.keras.Model,
                                               write_to_file: bool = True):
     """
     Convert a Keras model to a quantization-aware model, train and evaluate it
-    (applying default optimisation), and save it to a file.
+    (applying default optimisation), and (optionally) save it to a file.
 
     Args:
         model: The model to be converted. Can be trained or untrained
         train_dataset: The dataset to use for training the model
         test_dataset: The dataset to use for evaluating the model
+        write_to_file: Whether to save the converted model to a file
+
+    Returns:
+        The converted TFLite model in serialised format
     """
     from runner import compile_model, train_and_evaluate_tf
 
@@ -111,9 +126,16 @@ def evaluate_tflite_model(file_path: str, model: tf.keras.Model,
     tflite_interpreter.resize_tensor_input(output_details[0]['index'],(len(test_dataset_arr), num_classes))
     tflite_interpreter.allocate_tensors()
 
+    # Create input appropriate for TFLite model
+    test_dataset_samples_tflite = test_dataset_samples.copy()
+    if input_details[0]['dtype'] == np.uint8:
+      input_scale, input_zero_point = input_details[0]["quantization"]
+      test_dataset_samples_tflite = test_dataset_samples / input_scale + input_zero_point
+    test_dataset_samples_tflite = test_dataset_samples_tflite.astype(input_details[0]["dtype"])
+
     # Feed test data to input tensor
     input_details = tflite_interpreter.get_input_details()
-    tflite_interpreter.set_tensor(input_details[0]['index'], test_dataset_samples)
+    tflite_interpreter.set_tensor(input_details[0]['index'], test_dataset_samples_tflite)
 
     # Run predictions on TFLite and base model
     tflite_interpreter.invoke()
@@ -131,7 +153,6 @@ def evaluate_tflite_model(file_path: str, model: tf.keras.Model,
 
 if __name__ == "__main__":
     features, labels = data_processing.load_and_combine_data()
-    # features, labels = data_processing.preprocess_input(features, labels)
     train_dataset, test_dataset = data_processing.split_to_tf_datasets(features, labels)
     running_model = models.slam_cnn_padding_pyramid_lite(
         features[0].shape,
