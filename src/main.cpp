c99e397a285a1dda24e934d795c82422b058220c
Stijn van de Water <S.vandeWater-1@student.tudelft.nl>
2022-06-14 12:44:46 +0200

    Update diode calibration implementation

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,32 @@
 
 
 rtos::Thread calibration_thread;
+LightIntensityRegulator* regulator;
+
+void calibrate_diode_setup() {
+  regulator = new LightIntensityRegulator();
+
+  /**
+  while (regulator->resistorDown()) {
+    delay(1000);
+  }
+
+  while (regulator->resistorUp()) {
+    delay(1000);
+  }
+
+  digitalWrite(22, LOW);
+  **/
+
+  
+}
 
 void setup() {
   Serial.begin(9600);
   while(!Serial);
   calibration_thread.start(calibrate_diode_setup); // Should not be done multithreaded but is good for keeping the visualization.
 }
+
 
 void loop() {
   int r0 = analogRead(A0);
