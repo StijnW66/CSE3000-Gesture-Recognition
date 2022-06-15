405f53f144b55ab715a9ff8f6a3a3c70dfea1f93
mitko <mbarantiev@gmail.com>
2022-06-15 10:47:19 +0200

    Move plotting files to a new directory.

---
+++ src/receiver/plotting/plotting-util.h
@@ -0,0 +1,32 @@
+#ifndef PLOT_UTIL
+#define PLOT_UTIL
+
+#include "Arduino.h"
+#include "../receiver-parameters.h"
+
+template<typename T, int bl>
+void sendSignal(T signal[NUM_PDs][bl], int length) {
+    Serial.println("Start");
+    for (int i = 0; i < length; i++)
+    {
+        for (size_t pdId = 0; pdId < NUM_PDs; pdId++)
+        {
+            Serial.print(signal[pdId][i]);
+            Serial.print(" ");
+        }
+        Serial.println();
+    }
+    Serial.println("Done");
+}
+
+template<typename T, int bl>
+void sendSignal1(T signal[bl], int length) {
+    Serial.println("Start");
+    for (int i = 0; i < length; i++)
+    {
+        Serial.println(signal[i]);
+    }
+    Serial.println("Done");
+}
+
+#endif
--- src/receiver/receiver-util.h
+++ src/receiver/receiver-util.h
@@ -3,34 +3,8 @@
 
 #include "Arduino.h"
 #include "receiver-parameters.h"
+#include "plotting/plotting-util.h"
 
 #define FOR(i1, i2, L1, L2, codeline) for (int i1 = 0; i1 < L1; i1++) for (int i2 = 0; i2 < L2; i2++) codeline;
-
-template<typename T, int bl>
-void sendSignal(T signal[NUM_PDs][bl], int length) {
-    Serial.println("Start");
-    for (int i = 0; i < length; i++)
-    {
-        for (size_t pdId = 0; pdId < NUM_PDs; pdId++)
-        {
-            Serial.print(signal[pdId][i]);
-            Serial.print(" ");
-        }
-        Serial.println();
-    }
-    Serial.println("Done");
-}
-
-template<typename T, int bl>
-void sendSignal1(T signal[bl], int length) {
-    Serial.println("Start");
-    for (int i = 0; i < length; i++)
-    {
-        Serial.println(signal[i]);
-    }
-    Serial.println("Done");
-}
-
-
 
 #endif
